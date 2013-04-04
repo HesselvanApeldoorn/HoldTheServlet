ba15d6f3b137d7544cfb3fc7c917ef65bb2f7494
githubva <hesselva1@gmail.com>
2013-04-04 16:07:50 +0200

    force-pushed settings

--- src/model/GameRooms.java
+++ src/model/GameRooms.java
@@ -10,7 +10,7 @@ public class GameRooms {
 		ArrayList<String> names = new ArrayList<String>();
 		names.add("Bert");
 		names.add("Ernie");
-		GameRoom gameRoom = new GameRoom("128.0.0.1", 100, names );
+		GameRoom gameRoom = new GameRoom("129.0.0.1", 100, names );
 		rooms.add(gameRoom);
 	}
 }
